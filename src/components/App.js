b7629f6ccd096b231d422d9360a447ec99216208
Borrison <borissoto@gmail.com>
2020-11-22 23:58:14 -0400

    using custom hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
+import useVideos from '../hooks/useVideos';
 
 const App = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
 
-  // setSelectedVideo(response.data.items[0]);
+  useVideos[(videos, search)] = useVideos('buildings');
+
+  useEffect(() => {
+    setSelectedVideo(response.data.items[0]);
+  }, [videos]);
 
   return (
     <div className='ui container'>
-      <SearchBar onFormSubmit={onTermSubmit} />
+      <SearchBar onFormSubmit={search} />
       <div className='ui grid'>
         <div className='ui row'>
           <div className='eleven wide column'>
