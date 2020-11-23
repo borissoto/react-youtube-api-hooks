81023850ba6629b93378a225fa4cc9d7612030af
Borrison <borissoto@gmail.com>
2020-11-22 23:54:02 -0400

    custom hooks

---
+++ src/components/SearchBar.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    // TODO: Make sure we call
+    onFormSubmit(term);
+
+    // callback from parent component
+  };
+
+  return (
+    <div className='search-bar ui segment'>
+      <form onSubmit={onSubmit} className='ui form'>
+        <div className='field'>
+          <label>Video Search</label>
+          <input
+            type='text'
+            value={term}
+            onChange={(event) => setTerm(event.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
